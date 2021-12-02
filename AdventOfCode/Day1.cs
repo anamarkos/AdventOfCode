d64ab420b7e8e993e0ee69790531f89fd575a912
Anastasios Markos <amarkos@novibet.com>
2021-12-02 15:03:03 +0200

    Add project files.

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    internal class Day1
+    {
+        public Day1()
+        {
+            int[] msrmnts = Data.DepthMesurements;
+
+            var myResults = msrmnts.Skip(1)
+                .Select((ms, index) => new { ms, index })
+                .Where(x => x.ms > msrmnts[x.index])
+                .Count();
+
+            int count = 0;
+            msrmnts.Select((ms, index) => new {ms,index}).Skip(1).ToList()
+                   .ForEach(x => { if (x.ms > msrmnts[x.index - 1]) { count++; } });
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode;
+
+//Day 1 Part2
+int[] msrmnts = Data.DepthMesurements;
+List<int> chunkSums = new();
+
+int index = 0;
+int chunkRemains = msrmnts.Length % 3;
+
+while (true)
+{
+    if(index < msrmnts.Length - chunkRemains)
+    {
+        int sum = 0;
+
+        for (int i = index; i < index + 3; i++)
+        {
+            sum += msrmnts[i++];
+        }
+
+        chunkSums.Add(sum);
+        index++;
+    }
+    else
+    {
+        break;
+    }
+    
+}
+
+int countIn = 0;
+int countDe = 0;
+int countNc = 0;
+
+chunkSums.Select((mSum, index) => new { mSum, index }).Skip(1).ToList()
+       .ForEach(x => { if (x.mSum > chunkSums[x.index - 1]) { countIn++; } });
+
+chunkSums.Select((mSum, index) => new { mSum, index }).Skip(1).ToList()
+.ForEach(x => { if (x.mSum < chunkSums[x.index - 1]) { countDe++; } });
+
+chunkSums.Select((mSum, index) => new { mSum, index }).Skip(1).ToList()
+       .ForEach(x => { if (x.mSum == chunkSums[x.index - 1]) { countNc++; } });
+
+Console.WriteLine(countIn);
+
+
+
+//not 528
+//not 1416
+
